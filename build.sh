8494d17a654906d2be260275d345f3eb69b0db96
minecraft <minecraft@li91-166.members.linode.com>
2010-11-27 13:33:36 -0800

    Added build and cleane scripts for Unix machines.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+javac -cp ../Minecraft_Mod.jar TradeCraft.java
+
+jar cvf TradeCraft.jar *.class > /dev/null
+
+echo Built!
---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm *.class
+rm TradeCraft.jar
+
+echo Clean!
