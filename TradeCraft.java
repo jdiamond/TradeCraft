8bbb20365781a14a6b58ed1d6d404589f3c42311
Jason Diamond <jason@diamond.name>
2010-12-01 16:41:34 -0800

    Cleaned things up a bit.

--- TradeCraft.java
+++ TradeCraft.java
@@ -9,25 +9,21 @@ import java.util.regex.Pattern;
 
 public class TradeCraft extends Plugin {
 
-    // Some constants
+    // The plugin name.
     private static final String name = "TradeCraft";
-    private static final String version = "129";
-    private static final String configurationFileName = name + ".txt";
+
+    // The plugin version. The first part is the version of hMod this is built against.
+    // The second part is the release number built against that version of hMod.
+    // A "+" at the end means this is a development version that hasn't been released yet.
+    private static final String version = "129.1+";
 
     // Stuff used to interact with the server.
     private final Logger log = Logger.getLogger("Minecraft");
     private final Server server = etc.getServer();
+
+    // Objects used by the plugin.
+    private final TradeCraftConfigurationFile configuration = new TradeCraftConfigurationFile();
     private final TradeCraftListener listener = new TradeCraftListener();
-
-    // Stores the information read from the configuration file.
-    private final Map<String, TradeInfo> tradeInfo = new HashMap<String, TradeInfo>();
-
-    // Patterns used to parse the configuration file.
-    private static final Pattern commentPattern = Pattern.compile("^\\s*#.*$");
-    private static final Pattern infoPattern = Pattern.compile(
-            "^\\s*([^,]+)\\s*," + // name
-            "\\s*(\\d+)\\s*," + // id
-            "\\s*(\\d+):(\\d+)\\s*$"); // amount:value
 
     // Some data value constants.
     private static final int CHEST = 54;
@@ -35,30 +31,8 @@ public class TradeCraft extends Plugin {
     private static final int GOLD_INGOT = 266;
     private static final int MIXED = -1;
 
-    private static final int BLOCK_SIZE = 64;
-
-    // Represents a line parsed from the configuration file.
-    static class TradeInfo {
-        public String name;
-        public int id;
-        public int amount;
-        public int value;
-    }
-
-    // Represents what's inside a chest.
-    static class ChestInfo {
-        public int id;
-        public int total;
-
-        public void addItem(Item item) {
-            if (total == 0) {
-                id = item.getItemId();
-            } else if (id != item.getItemId()) {
-                id = MIXED;
-            }
-            total += item.getAmount();
-        }
-    }
+    // The maximum number of items that can be stacked in one slot.
+    private static final int MAX_STACK_SIZE = 64;
 
     public void enable() {
     }
@@ -69,7 +43,7 @@ public class TradeCraft extends Plugin {
     public void initialize() {
         log.info(name + " " + version + " initialized");
 
-        loadConfiguration();
+        configuration.load();
 
         etc.getLoader().addListener(
                 PluginLoader.Hook.BLOCK_RIGHTCLICKED,
@@ -78,52 +52,83 @@ public class TradeCraft extends Plugin {
                 PluginListener.Priority.MEDIUM);
     }
 
-    private void loadConfiguration() {
-        try {
-            tradeInfo.clear();
+    private void sendMessage(Player player, String format, Object... args) {
+        String message = String.format(format, args);
+        player.sendMessage(message);
+    }
 
-            BufferedReader configurationFile = new BufferedReader(new FileReader(configurationFileName));
+    // Represents the TradeCraft.txt configuration file.
+    private class TradeCraftConfigurationFile {
+        private static final String configurationFileName = name + ".txt";
+        private final Map<String, TradeInfo> tradeInfo = new HashMap<String, TradeInfo>();
 
-            int lineNumber = 0;
-            String line;
+        // Patterns used to parse the configuration file.
+        private final Pattern commentPattern = Pattern.compile("^\\s*#.*$");
+        private final Pattern infoPattern = Pattern.compile(
+                "^\\s*([^,]+)\\s*," + // name
+                "\\s*(\\d+)\\s*," + // id
+                "\\s*(\\d+):(\\d+)\\s*$"); // amount:value
 
-            while ((line = configurationFile.readLine()) != null) {
-                lineNumber += 1;
+        private void load() {
+            try {
+                tradeInfo.clear();
 
-                if (line.trim().equals("")) {
-                    continue;
+                BufferedReader configurationFile = new BufferedReader(new FileReader(configurationFileName));
+
+                int lineNumber = 0;
+                String line;
+
+                while ((line = configurationFile.readLine()) != null) {
+                    lineNumber += 1;
+
+                    if (line.trim().equals("")) {
+                        continue;
+                    }
+
+                    Matcher commentMatcher = commentPattern.matcher(line);
+
+                    if (commentMatcher.matches()) {
+                        continue;
+                    }
+
+                    Matcher infoMatcher = infoPattern.matcher(line);
+
+                    if (!infoMatcher.matches()) {
+                        log.warning(
+                                "Failed to parse line number " + lineNumber +
+                                " in " + configurationFileName +
+                                ": " + line);
+                        continue;
+                    }
+
+                    TradeInfo info = new TradeInfo();
+                    info.name = infoMatcher.group(1);
+                    info.id = Integer.parseInt(infoMatcher.group(2));
+                    info.amount = Integer.parseInt(infoMatcher.group(3));
+                    info.value = Integer.parseInt(infoMatcher.group(4));
+                    tradeInfo.put("[" + info.name.toUpperCase() + "]", info);
                 }
 
-                Matcher commentMatcher = commentPattern.matcher(line);
-
-                if (commentMatcher.matches()) {
-                    continue;
-                }
-
-                Matcher infoMatcher = infoPattern.matcher(line);
-
-                if (!infoMatcher.matches()) {
-                    log.warning(
-                            "Failed to parse line number " + lineNumber +
-                            " in " + configurationFileName +
-                            ": " + line);
-                    continue;
-                }
-
-                TradeInfo info = new TradeInfo();
-                info.name = infoMatcher.group(1);
-                info.id = Integer.parseInt(infoMatcher.group(2));
-                info.amount = Integer.parseInt(infoMatcher.group(3));
-                info.value = Integer.parseInt(infoMatcher.group(4));
-                tradeInfo.put("[" + info.name.toUpperCase() + "]", info);
+                configurationFile.close();
+            } catch (IOException e) {
+                log.warning("Error reading " + configurationFileName);
             }
+        }
 
-            configurationFile.close();
-        } catch (IOException e) {
-            log.warning("Error reading TradeCraft.txt");
+        public TradeInfo get(String name) {
+            return tradeInfo.get(name.toUpperCase());
         }
     }
 
+    // Represents a line parsed from the configuration file.
+    static class TradeInfo {
+        public String name;
+        public int id;
+        public int amount;
+        public int value;
+    }
+
+    // This is where most of the work is done.
     private class TradeCraftListener extends PluginListener {
 
         public void onBlockRightClicked(
@@ -139,42 +144,43 @@ public class TradeCraft extends Plugin {
             int y = blockClicked.getY();
             int z = blockClicked.getZ();
 
-            Sign sign = (Sign) server.getComplexBlock(x, y, z);
-            TradeInfo currentTradeInfo = getTradeInfo(sign);
+            Sign sign = (Sign)server.getComplexBlock(x, y, z);
+            TradeInfo tradeInfo = getTradeInfo(sign);
 
-            if (currentTradeInfo == null) {
+            if (tradeInfo == null) {
                 return;
             }
 
-            Block block = (Block) server.getBlockAt(x, y - 1, z);
+            Block block = (Block)server.getBlockAt(x, y - 1, z);
 
             if (block.getType() != CHEST) {
                 return;
             }
 
-            Chest chest = (Chest) server.getComplexBlock(x, y - 1, z);
-            ChestInfo chestInfo = getChestInfo(chest);
+            Chest chest = (Chest)server.getComplexBlock(x, y - 1, z);
+            ChestInfo chestInfo = new ChestInfo(chest);
 
             if (chestInfo.total == 0) {
-                player.sendMessage(
-                        "Exchange rate for " + currentTradeInfo.name +
-                        " is " + currentTradeInfo.amount + " items" +
-                        " for " + currentTradeInfo.value + " gold.");
-                player.sendMessage("The chest is empty.");
+                sendMessage(player,
+                            "Exchange rate for %1$s is %2$d items for %3$d gold.",
+                            tradeInfo.name,
+                            tradeInfo.amount,
+                            tradeInfo.value);
+                sendMessage(player, "The chest is empty.");
                 return;
             }
 
-            if (chestInfo.id == MIXED) {
-                player.sendMessage("The chest has more than one type of item in it!");
+            if (!chestInfo.containsOnlyOneItemType()) {
+                sendMessage(player, "The chest has more than one type of item in it!");
                 return;
             }
 
             if (chestInfo.id == GOLD_INGOT) {
-                makePurchase(player, currentTradeInfo, chestInfo, chest);
-            } else if (chestInfo.id == currentTradeInfo.id) {
-                makeSale(player, currentTradeInfo, chestInfo, chest);
+                makePurchase(player, tradeInfo, chestInfo, chest);
+            } else if (chestInfo.id == tradeInfo.id) {
+                makeSale(player, tradeInfo, chestInfo, chest);
             } else {
-                player.sendMessage("You can't sell that here!");
+                sendMessage(player, "You can't sell that here!");
             }
 
             return;
@@ -183,7 +189,7 @@ public class TradeCraft extends Plugin {
         private TradeInfo getTradeInfo(Sign sign) {
             for (int i = 0; i < 4; i++) {
                 String signText = sign.getText(i);
-                TradeInfo currentTradeInfo = tradeInfo.get(signText.toUpperCase());
+                TradeInfo currentTradeInfo = configuration.get(signText);
 
                 if (currentTradeInfo != null) {
                     return currentTradeInfo;
@@ -191,19 +197,6 @@ public class TradeCraft extends Plugin {
             }
 
             return null;
-        }
-
-        private ChestInfo getChestInfo(Chest chest) {
-            ChestInfo info = new ChestInfo();
-
-            for (hm realItem : chest.getArray()) {
-                if (realItem != null) {
-                    Item item = new Item(realItem);
-                    info.addItem(item);
-                }
-            }
-
-            return info;
         }
 
         private void makePurchase(
@@ -215,18 +208,17 @@ public class TradeCraft extends Plugin {
             int totalItems = chestInfo.total / currentTradeInfo.value * currentTradeInfo.amount;
 
             if (totalItems == 0) {
-                player.sendMessage(
-                        "You need to spend at least " +
-                        currentTradeInfo.value +
-                        " gold to get any items.");
+                sendMessage(player,
+                            "You need to spend at least %1$d gold to get any items.",
+                            currentTradeInfo.value);
                 return;
             }
 
-            player.sendMessage(
-                    "You bought " + totalItems + " " +
-                    currentTradeInfo.name +
-                    " for " + chestInfo.total +
-                    " gold.");
+            sendMessage(player,
+                        "You bought %1$d %2$s for %3$d gold.",
+                        totalItems,
+                        currentTradeInfo.name,
+                        chestInfo.total);
 
             populateChest(chest, currentTradeInfo.id, totalItems);
         }
@@ -240,18 +232,18 @@ public class TradeCraft extends Plugin {
             int totalValue = chestInfo.total / currentTradeInfo.amount * currentTradeInfo.value;
 
             if (totalValue == 0) {
-                player.sendMessage(
-                        "You need to sell at least " +
-                        currentTradeInfo.amount +
-                        " to get any gold.");
+                sendMessage(player,
+                            "You need to sell at least %1$d %2$s to get any gold.",
+                            currentTradeInfo.amount,
+                            currentTradeInfo.name);
                 return;
             }
 
-            player.sendMessage(
-                    "You sold " + chestInfo.total + " " +
-                    currentTradeInfo.name +
-                    " for " + totalValue +
-                    " gold.");
+            sendMessage(player,
+                        "You sold %1$d %2$s for %3$d gold.",
+                        chestInfo.total,
+                        currentTradeInfo.name,
+                        totalValue);
 
             populateChest(chest, GOLD_INGOT, totalValue);
         }
@@ -259,19 +251,47 @@ public class TradeCraft extends Plugin {
         private void populateChest(Chest chest, int id, int amount) {
             chest.clearContents();
 
-            int blocks = amount / BLOCK_SIZE;
+            int blocks = amount / MAX_STACK_SIZE;
 
             for (int i = 0; i < blocks; i++) {
-                chest.addItem(new Item(id, BLOCK_SIZE));
+                chest.addItem(new Item(id, MAX_STACK_SIZE));
             }
 
-            int remainder = amount % BLOCK_SIZE;
+            int remainder = amount % MAX_STACK_SIZE;
 
             if (remainder > 0) {
                 chest.addItem(new Item(id, remainder));
             }
 
             chest.update();
+        }
+    }
+
+    // Represents what's inside a chest.
+    static class ChestInfo {
+        public int id;
+        public int total;
+
+        public ChestInfo(Chest chest) {
+            for (hm realItem : chest.getArray()) {
+                if (realItem != null) {
+                    Item item = new Item(realItem);
+                    addItem(item);
+                }
+            }
+        }
+
+        private void addItem(Item item) {
+            if (total == 0) {
+                id = item.getItemId();
+            } else if (id != item.getItemId()) {
+                id = MIXED;
+            }
+            total += item.getAmount();
+        }
+
+        public boolean containsOnlyOneItemType() {
+            return id != MIXED;
         }
     }
 }
