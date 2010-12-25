494cbfd454ad9e4118998007ed3c1ecf04dba71b
Jason Diamond <jason@diamond.name>
2010-12-25 14:57:27 -0800

    Updated for hMod 132.

--- TradeCraft.java
+++ TradeCraft.java
@@ -15,7 +15,7 @@ public class TradeCraft extends Plugin {
     // The plugin version. The first part is the version of hMod this is built against.
     // The second part is the release number built against that version of hMod.
     // A "+" at the end means this is a development version that hasn't been released yet.
-    private static final String version = "131.2";
+    private static final String version = "132.1";
 
     // Stuff used to interact with the server.
     private final Logger log = Logger.getLogger("Minecraft");
@@ -303,15 +303,15 @@ public class TradeCraft extends Plugin {
         public int total;
 
         public ChestInfo(Chest chest) {
-            for (hn realItem : chest.getArray()) {
-                if (realItem != null) {
-                    Item item = new Item(realItem);
-                    addItem(item);
-                }
+            for (Item item : chest.getContents()) {
+                addItem(item);
             }
         }
 
         private void addItem(Item item) {
+            if (item == null) {
+                return;
+            }
             if (total == 0) {
                 id = item.getItemId();
             } else if (id != item.getItemId()) {
